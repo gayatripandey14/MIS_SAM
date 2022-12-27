b119dce1f09b47adbc63c02b149487cb937566c1
gayatripandey14 <gayatripandey819@gmail.com>
2022-12-27 14:51:16 +0530

    analytics username change

--- analytics/views.py
+++ analytics/views.py
@@ -32,13 +32,13 @@ class GetChildrensView(GenericAPIView):
         user = request.user
         if user.user_type == 'Super Admin':
             
-            user_obj = AccountsSmppusers.objects.all()
+            user_obj = SmppUser.objects.all()
             rtrn = []
 
             for username in user_obj:
                 data_dict = {}
 
-                username = username.smpp_userdetails_id
+                username = username.system_id
                 data_dict["username"]=username
                 rtrn.append(data_dict)
             return Response(rtrn)
